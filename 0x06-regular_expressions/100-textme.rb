b11db4675a343c3bbe7ecb2296d9bd1fb2e6a61d
gjdame <gregoryjdame@gmail.com>
2018-06-18 19:59:58 +0000

    initial add 100

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+my_string = ARGV[0].scan(/\[from:(.*?)\]|\[to:(.*?)\]|\[flags:(.*?)\]/).join(" ")
+my_string = my_string.squeeze
+my_list = my_string.split
+puts my_list.join(',')
