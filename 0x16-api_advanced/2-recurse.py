dcedbb9473fbef35531d1f3c6cfe8851f338899b
gjdame <gregoryjdame@gmail.com>
2018-08-30 21:45:45 +0000

    add 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,11 +9,14 @@ def recurse(subreddit, hot_list=[], after=None):
     sub = requests.get('http://www.reddit.com/r/{}/hot.json?after={}'
                        .format(subreddit, after), headers=user_agent)
 
-    sub = sub.json().get('data')
-    after = sub.get('after')
-    sub = sub.get('children')
-    for obj in sub:
-        hot_list.append(obj['data'].get('title'))
-    if after not None:
-        recurse(subreddit, hot_list, after)
-    return(hot_list)
+    try:
+        sub = sub.json().get('data')
+        after = sub.get('after')
+        sub = sub.get('children')
+        for obj in sub:
+            hot_list.append(obj['data'].get('title'))
+        if after not None:
+            recurse(subreddit, hot_list, after)
+        return(hot_list)
+    except:
+        return(None)
