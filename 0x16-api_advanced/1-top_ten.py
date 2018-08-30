6895b8514e23fc571265339ed7bdf8cd8f623c87
gjdame <gregoryjdame@gmail.com>
2018-08-30 20:26:16 +0000

    add 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+displays top 10 hot posts
+'''
+import requests
+
+
+def top_ten(subreddit):
+    user_agent = {'User-agent': 'greg'}
+    sub = requests.get('http://www.reddit.com/r/{}/hot.json'
+                       .format(subreddit), headers=user_agent)
+
+    sub = sub.json().get('data')
+    sub = sub.get('children')
+    i = 0
+    try:
+        for obj in sub:
+            if i > 9:
+                break
+            print(obj['data'].get('title'))
+            i += 1
+
+    except:
+        return(None)
