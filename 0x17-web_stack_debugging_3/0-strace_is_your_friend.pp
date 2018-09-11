17cbdda0fedd8701c3d5dfd3ad737ce566847e86
gjdame <gregoryjdame@gmail.com>
2018-09-11 20:36:30 +0000

    initialize README and 0

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+exec { replace:
+     command => '/bin/sed -ie \'s/class-wp-locale.phpp/class-wp-locale.php/\' /var/www/html/wp-settings.php'
+}
+
+exec { restart:
+     command    => "/etc/init.d/apache2 restart"
+}
